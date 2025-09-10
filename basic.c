e85d7c37bf9637e947bc516063a00b4e58f30fac
Mishra Ji <mishramanish9119@gmail.com>
2025-09-10 13:25:56 +1245

    updated

--- basic.c
+++ basic.c
@@ -4,7 +4,8 @@ int main(){
     a=6;
     b=6;
     c=a+b;
-    printf("The sum of two numers: %d",c);
+    printf("The sum of two numers: %d \n",c);
+    printf("welcome to new world");
     return 0;
     
 }
